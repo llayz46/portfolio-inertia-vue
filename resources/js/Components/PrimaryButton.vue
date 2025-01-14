7f64cec0e4b97e87938f2095fb929c2338d6b699
llayz46 <layzer@outlook.fr>
2025-01-14 17:09:51 +0100

    style(components): Add hover effect on PrimaryButton.vue and refacto span to ::before and ::after

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -1,6 +1,5 @@
 <template>
-    <button class="relative text-base font-semibold text-white py-2 px-5 bg-gradient-to-r from-primary-800 to-primary-600 rounded-lg">
-        <span class="absolute -inset-[2px] bg-gradient-to-r from-primary-800/15 to-primary-600/15 rounded-[10px]" aria-hidden="true"></span>
+    <button class="relative text-base font-semibold text-white py-2 px-5 bg-gradient-to-r from-primary-800 to-primary-600 rounded-lg w-fit before:pointer-events-none before:absolute before:inset-0 before:bg-black/30 before:opacity-0 before:hover:opacity-100 before:rounded-lg before:transition-opacity after:pointer-events-none after:absolute after:-inset-[2px] after:bg-gradient-to-r after:from-primary-800/15 after:to-primary-600/15 after:rounded-[10px]">
         <slot/>
     </button>
 </template>
