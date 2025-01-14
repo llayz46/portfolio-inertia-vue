1b09dd650ffb99f1690ad4d343ce292b9560fa47
llayz46 <layzer@outlook.fr>
2025-01-14 15:23:00 +0100

    feat(routes): add routes, controller and view for Landing.vue & delete Welcome.vue

---
+++ resources/js/Pages/Landing.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import Navbar from "@/Components/Navbar.vue";
+</script>
+
+<template>
+    <Head title="Grr paa" />
+
+    <div class="test h-screen w-full pt-3 bg-[#091821]">
+        <Navbar />
+    </div>
+</template>
