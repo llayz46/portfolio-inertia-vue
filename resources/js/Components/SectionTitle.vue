67b0578010d89db9e3ee49d9293862774487ce64
llayz46 <layzer@outlook.fr>
2025-01-14 16:56:10 +0100

    style(components): Add SectionTitle.vue

---
+++ resources/js/Components/SectionTitle.vue
@@ -0,0 +1,5 @@
+<template>
+    <h2 class="text-4xl font-medium tracking-[-0.045em] bg-gradient-to-t from-neutral-700 to-white to-60% bg-clip-text text-transparent">
+        <slot />
+    </h2>
+</template>
