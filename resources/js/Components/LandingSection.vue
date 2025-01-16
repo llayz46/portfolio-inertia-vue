51dd7e65b85e476613d63aacf184a504e6c27967
llayz46 <layzer@outlook.fr>
2025-01-16 11:04:08 +0100

    style(components): Create a LandingSection.vue component

---
+++ resources/js/Components/LandingSection.vue
@@ -0,0 +1,9 @@
+<template>
+    <section class="max-w-4xl mx-auto pt-4">
+        <h2 class="text-4xl font-medium tracking-small bg-gradient-to-t from-neutral-700 to-white to-60% bg-clip-text text-transparent mb-8">
+            <slot name="title" />
+        </h2>
+
+        <slot />
+    </section>
+</template>
--- resources/js/Components/SectionTitle.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <h2 class="text-4xl font-medium tracking-small bg-gradient-to-t from-neutral-700 to-white to-60% bg-clip-text text-transparent">
-        <slot />
-    </h2>
-</template>
