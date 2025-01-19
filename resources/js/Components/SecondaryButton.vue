318e8d196c1f828b51d848b5144561dd66fefff2
llayz46 <layzer@outlook.fr>
2025-01-19 19:45:33 +0100

    style: Add hover effect on SecondaryButton.vue component

--- resources/js/Components/SecondaryButton.vue
+++ resources/js/Components/SecondaryButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="px-5 py-2 rounded-lg bg-gradient-to-t from-white/85 to-white/95 hover:opacity-80 secondary-button-shadow font-dm-sans font-semibold text-xs text-background transition-opacity">
+    <button class="px-5 py-2 rounded-lg bg-gradient-to-t from-white/85 to-white/95 hover:opacity-80 secondary-button-shadow font-dm-sans font-semibold text-xs text-background transition-opacity whitespace-nowrap">
         <slot />
     </button>
 </template>
