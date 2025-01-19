bf1512de1f89f326f5534dbef995e02f149138b2
llayz46 <layzer@outlook.fr>
2025-01-19 19:44:27 +0100

    style(fix): reduce index

--- resources/js/Components/WorkflowStepBadge.vue
+++ resources/js/Components/WorkflowStepBadge.vue
@@ -1,5 +1,5 @@
 <template>
-    <span class="relative flex justify-center items-center z-50 size-[50px] rounded-full bg-gradient-to-tr from-primary-300 to-primary-900 text-2xl font-medium workflow-step-badge-inset-shadow before:pointer-events-none before:absolute before:inset-0 before:size-full before:rounded-full before:bg-white/10 after:pointer-events-none after:-z-10 after:absolute after:inset-0 after:size-full after:bg-red-500 after:rounded-full after:bg-gradient-to-tr after:from-primary-300 after:to-primary-900 after:blur-md">
+    <span class="relative flex justify-center items-center z-20 size-[50px] rounded-full bg-gradient-to-tr from-primary-300 to-primary-900 text-2xl font-medium workflow-step-badge-inset-shadow before:pointer-events-none before:absolute before:inset-0 before:size-full before:rounded-full before:bg-white/10 after:pointer-events-none after:-z-10 after:absolute after:inset-0 after:size-full after:bg-red-500 after:rounded-full after:bg-gradient-to-tr after:from-primary-300 after:to-primary-900 after:blur-md">
         <slot />
     </span>
 </template>
