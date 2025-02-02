988e3d9fcda6b7ab9c6cf841218a8173f07ca6b4
llayz46 <layzer@outlook.fr>
2025-02-02 13:17:50 +0100

    fix: Project table : description column text to longtext

--- database/migrations/2025_01_22_134206_create_projects_table.php
+++ database/migrations/2025_01_22_134206_create_projects_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description');
+            $table->longText('description');
             $table->string('image');
             $table->string('slug')->unique();
             $table->string('url');
